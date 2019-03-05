2467c757800f3a29150eded01cca42d049c9fac2
Simone Vittori <hello@simonewebdesign.it>
2019-03-05 14:17:44 +0000

    Implement neighborsCount and shouldLive

--- life/Life.swift
+++ life/Life.swift
@@ -1,5 +1,34 @@
+typealias Cells = [Cell]
+
+//struct Cell: Hashable {
+//    let x: Int
+//    let y: Int
+//}
+
+typealias Cell = (x: Int, y: Int)
+
 struct Life {
-    static func sum() -> Int {
-        return 2
+    // these are the numbers to add/subtract from a coordinate in order
+    // to get the relative neighbour
+    static let offsets =             [(-1, -1), (-1, 0), (-1, 1), (0, -1),         (0, 1), (1, -1), (1, 0), (1, 1)]
+    static let offsetsIncludingOwn = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+//    private func aliveCellAndNeighbors(_cells: Cells) ->
+
+    private func shouldLive(isAlive: Bool, numOfNeighbors: Int) -> Bool {
+        return (numOfNeighbors == 2 && isAlive) || numOfNeighbors == 3
+    }
+
+    private func neighborsCount(_cells: Cells, _cell: Cell) -> Int {
+        return Life.offsets.reduce(0) { _acc, _c in
+            let x = _cell.0 + _c.0
+            let y = _cell.1 + _c.1
+
+            if _cells.contains(where: { $0.x == x && $0.y == y }) {
+                return _acc + 1
+            } else {
+                return _acc
+            }
+        }
     }
 }
--- lifeTests/LifeTests.swift
+++ lifeTests/LifeTests.swift
@@ -1,16 +1,7 @@
-//
-//  lifeTests.swift
-//  lifeTests
-//
-//  Created by Owen Worley on 04/03/2019.
-//  Copyright © 2019 lifeorg. All rights reserved.
-//
-
 import XCTest
 @testable import life
 
 class LifeTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,20 +10,10 @@ class LifeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testSum() {
-        XCTAssertEqual(Life.sum(), 2)
-    }
-
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
