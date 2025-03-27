70cc4ca934672364f2c7b36d798cd70eff46f1e5
Simone <hello@simonewebdesign.it>
2025-03-27 11:15:27 +0000

    Add tests

--- lifeTests/LifeTests.swift
+++ lifeTests/LifeTests.swift
@@ -2,18 +2,55 @@ import XCTest
 @testable import life
 
 class LifeTests: XCTestCase {
+    var life: Life!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        life = Life()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        life = nil
+        super.tearDown()
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSingleCellDies() {
+        // A single cell should die in the next generation
+        let initialCells: Cells = [Cell(0, 0)]
+        let nextGeneration = life.tick(cells: initialCells)
+        XCTAssertTrue(nextGeneration.isEmpty)
+    }
+
+    func testBlockPatternStaysStable() {
+        // A 2x2 block should remain unchanged
+        let block: Cells = [
+            Cell(0, 0), Cell(0, 1),
+            Cell(1, 0), Cell(1, 1)
+        ]
+        let nextGeneration = life.tick(cells: block)
+        XCTAssertEqual(nextGeneration, block)
+    }
+
+    func testBlinkerPatternOscillates() {
+        // A blinker (3 cells in a row) should oscillate between horizontal and vertical
+        let horizontalBlinker: Cells = [
+            Cell(0, 0), Cell(0, 1), Cell(0, 2)
+        ]
+
+        let nextGeneration = life.tick(cells: horizontalBlinker)
+        let expectedVerticalBlinker: Cells = [
+            Cell(-1, 1), Cell(0, 1), Cell(1, 1)
+        ]
+        XCTAssertEqual(nextGeneration, expectedVerticalBlinker)
+
+        // And back to horizontal
+        let backToHorizontal = life.tick(cells: nextGeneration)
+        XCTAssertEqual(backToHorizontal, horizontalBlinker)
+    }
+
+    func testEmptyGridStaysEmpty() {
+        let emptyGrid: Cells = []
+        let nextGeneration = life.tick(cells: emptyGrid)
+        XCTAssertTrue(nextGeneration.isEmpty)
     }
 }
